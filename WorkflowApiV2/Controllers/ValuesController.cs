4a044cbb00d96572795bdbae9319e28b44bb6a79
Carlos Grappa <cgrappa@gmail.com>
2017-10-22 21:54:54 -0300

    Test of deploy scripts

--- WorkflowApiV2/Controllers/ValuesController.cs
+++ WorkflowApiV2/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace WorkflowApiV2.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value-"+id;
         }
 
         // POST api/values
